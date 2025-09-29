c32bb69d7b32d074b77d2b39a47f92e7aef32dd0
Gustavo Russeff <g.russ3ff@gmail.com>
2025-09-29 20:33:02 -0300

    feat: Adicionar projeto Exemplo 2 com testes otimizados

---
+++ src/test/java/TestCalculadora.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestCalculadora {
+
+    @Test
+    void testGetSoma() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 6.0;
+        double retornoFeito = calculadora.getSoma();
+        assertEquals(retornoEsperado, retornoFeito, 2);
+    }
+
+    @Test
+    void testGetDiferenca() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getDiferenca();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+    @Test
+    void testGetProduto() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 8.0;
+        double retornoFeito = calculadora.getProduto();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+    @Test
+    void testGetQuociente() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getQuociente();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+}
